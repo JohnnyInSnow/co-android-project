02188f21f11339452dd417b5470150bbaeed4061
櫻雪紛飛 <good258johnny@yahoo.com.tw>
2015-12-20 16:12:55 +0800

    setting page radioButtion for group complementation
    完成setting頁面 radiobButton 多選一功能
    
    Share preference function is not done.

--- ActionTabs/app/build/generated/source/r/debug/cs/hung/R.java
+++ ActionTabs/app/build/generated/source/r/debug/cs/hung/R.java
@@ -11,11 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int appmainpage=0x7f020000;
-        public static final int group=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int student=0x7f020003;
-        public static final int teacher=0x7f020004;
+        public static final int appbackpage=0x7f020000;
+        public static final int appmainpage=0x7f020001;
+        public static final int group=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int student=0x7f020004;
+        public static final int teacher=0x7f020005;
     }
     public static final class id {
         public static final int btnLevelSetting=0x7f06000e;
