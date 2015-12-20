dcad8452ad1e50657ed87b460da1ec21282155f5
櫻雪紛飛 <good258johnny@yahoo.com.tw>
2015-12-20 15:04:35 +0800

    Add the initialization page for app
    加入app初始畫面

--- ActionTabs/app/build/generated/source/r/debug/cs/hung/R.java
+++ ActionTabs/app/build/generated/source/r/debug/cs/hung/R.java
@@ -18,33 +18,35 @@ public final class R {
         public static final int teacher=0x7f020004;
     }
     public static final class id {
-        public static final int btnLevelSetting=0x7f06000d;
-        public static final int button=0x7f060004;
-        public static final int button2=0x7f060007;
-        public static final int editText=0x7f060006;
-        public static final int fragment_container=0x7f060000;
-        public static final int levelGroup=0x7f060009;
-        public static final int menuitem_about=0x7f060013;
-        public static final int menuitem_add=0x7f060011;
-        public static final int menuitem_feedback=0x7f060012;
-        public static final int menuitem_quit=0x7f060014;
-        public static final int menuitem_search=0x7f06000f;
-        public static final int menuitem_send=0x7f060010;
-        public static final int radioButton=0x7f06000a;
-        public static final int radioButton2=0x7f06000b;
-        public static final int radioButton3=0x7f06000c;
-        public static final int textView=0x7f060001;
-        public static final int textView12=0x7f060008;
-        public static final int textView2=0x7f060002;
-        public static final int textView3=0x7f060003;
-        public static final int textView4=0x7f060005;
-        public static final int textView9=0x7f06000e;
+        public static final int btnLevelSetting=0x7f06000e;
+        public static final int button=0x7f060005;
+        public static final int button2=0x7f060008;
+        public static final int editText=0x7f060007;
+        public static final int fragment_container=0x7f060001;
+        public static final int imageButton=0x7f060000;
+        public static final int levelGroup=0x7f06000a;
+        public static final int menuitem_about=0x7f060014;
+        public static final int menuitem_add=0x7f060012;
+        public static final int menuitem_feedback=0x7f060013;
+        public static final int menuitem_quit=0x7f060015;
+        public static final int menuitem_search=0x7f060010;
+        public static final int menuitem_send=0x7f060011;
+        public static final int radioButton=0x7f06000b;
+        public static final int radioButton2=0x7f06000c;
+        public static final int radioButton3=0x7f06000d;
+        public static final int textView=0x7f060002;
+        public static final int textView12=0x7f060009;
+        public static final int textView2=0x7f060003;
+        public static final int textView3=0x7f060004;
+        public static final int textView4=0x7f060006;
+        public static final int textView9=0x7f06000f;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int result=0x7f030001;
-        public static final int setting=0x7f030002;
-        public static final int studentfragment=0x7f030003;
+        public static final int init=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int result=0x7f030002;
+        public static final int setting=0x7f030003;
+        public static final int studentfragment=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f050000;
