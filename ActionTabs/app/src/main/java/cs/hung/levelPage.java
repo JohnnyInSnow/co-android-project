b7551751e9cce71b75497d026131e75ba2dc4345
櫻雪紛飛 <good258johnny@yahoo.com.tw>
2016-01-04 08:34:51 +0800

    加入關卡一layout 開始進行測試
    先測試levelPage.java 是否能指定fragment

--- ActionTabs/app/src/main/java/cs/hung/levelPage.java
+++ ActionTabs/app/src/main/java/cs/hung/levelPage.java
@@ -2,7 +2,10 @@ package cs.hung;
 
 import cs.hung.R;
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +13,24 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class levelPage extends Fragment {
-	
+    View root;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.studentfragment, container, false);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String p1 = prefs.getString("p1Name", "");
+        String p2 = prefs.getString("p2Name", "");
+        int level = prefs.getInt("level", 0);
+        if((p1.equals("")) || (p2.equals("")) || (level == 0)){
+            root = inflater.inflate(R.layout.studentfragment, container, false);
+            Log.e("message", "沒有完成輸入資料");
+        }else{
+            root = inflater.inflate(R.layout.game1layout, container, false);
+            Log.e("message", "有完成輸入資料");
+
+        }
+
+
 
 
         return root;
